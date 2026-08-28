b5ea49beca56ebfcd961579eb260f82bac059862
Sibghatullah <ssibghatkkhan81@gmail.com>
2026-08-28 19:04:05 +0530

    checking build working or not

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
 export default function Home({ currentWindow }: any) {
   return (
-    <div className="mt-10 mx-2 flex flex-col h-full">Home, {currentWindow}</div>
+    <div className="mt-10 mx-2 flex flex-col h-full">
+      Testing build, {currentWindow}
+    </div>
   );
 }
